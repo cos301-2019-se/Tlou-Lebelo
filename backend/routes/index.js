5fbd0802df7af7f26b2a9f46f9d845e8ee83fc14
Tlou Lebelo <u15209190@tuks.co.za>
2019-08-14 01:37:19 +0200

    new data api

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import path from 'path' ;
-import claimsController from '../controllers/claims.js';
+
 import usersController from '../controllers/users.js';
+import vehicleController from '../controllers/vehicle.js';
+import residenceController from '../controllers/residence.js'
+import claimsController from '../controllers/claims.js';
+import settingsController from '../controllers/settings.js'
 
 const router = express.Router();
 
@@ -12,12 +16,33 @@ router.post('/api/v1/user', usersController.createUser);
 router.put('/api/v1/user/:id', usersController.updateUser);
 router.delete('/api/v1/user/:id', usersController.deleteUser);
 
-//2. Claims Controllers
+//2. Vehicle Controllers
+router.get('/api/v1/vehicle', vehicleController.getAllVehicle);
+router.get('/api/v1/vehicle/:id', vehicleController.getVehicle);
+router.post('/api/v1/vehicle', vehicleController.createVehicle);
+router.put('/api/v1/vehicle/:id', vehicleController.updateVehicle);
+router.delete('/api/v1/vehicle/:id', vehicleController.deleteVehicle);
+
+//3. Residence Controllers
+router.get('/api/v1/residence', residenceController.getAllResidence);
+router.get('/api/v1/residence/:id', residenceController.getResidence);
+router.post('/api/v1/residence', residenceController.createResidence);
+router.put('/api/v1/residence/:id', residenceController.updateResidence);
+router.delete('/api/v1/residence/:id', residenceController.deleteResidence);
+
+//4. Claims Controllers
 router.get('/api/v1/claim', claimsController.getAllClaims);
 router.get('/api/v1/claim/:id', claimsController.getClaim);
 router.post('/api/v1/claim', claimsController.createClaim);
 router.put('/api/v1/claim/:id', claimsController.updateClaim);
 router.delete('/api/v1/claim/:id', claimsController.deleteClaim);
+
+//5. Settings Controllers
+router.get('/api/v1/setting', settingsController.getAllSettings);
+router.get('/api/v1/setting/:id', settingsController.getSettings);
+router.post('/api/v1/setting', settingsController.createSettings);
+router.put('/api/v1/setting/:id', settingsController.updateSettings);
+router.delete('/api/v1/setting/:id', settingsController.deleteSettings);
 
 //3. User Interface
 router.get('/', function(req,res){
